9c8a318b548f80262d092664eb02830917b3086b
Jieun Lee <najee0610@naver.com>
2022-07-28 03:07:04 +0900

    feature: korail 라우터 생성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Home from "./Components/Home/Home";
-
 function App() {
   return <Home />;
 }
-
 export default App;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -18,5 +18,4 @@ function Home() {
     </div>
   );
 }
-
 export default Home;
