a81e63229dd4bb485a9f9a8138400a543573ed59
Jieun Lee <najee0610@naver.com>
2022-08-01 01:24:32 +0900

    feat: 코레일톡 메인화면 UI 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Main from "./Components/Korail/Main";
+// import Main from "./Components/Korail/alignPrac";
 
 function App() {
   return <Main />;
--- src/Components/Korail/Main.js
+++ src/Components/Korail/Main.js
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 import Switch from './images/MainSwitchBtn.svg';
 import Train from './images/MainTrain.svg';
 import Ticket from './images/MainTicket.svg';
+import Arrow from './images/BlueArrow.svg';
+import NoneTicket from './images/NoneClickTicketBtn.svg';
+import NoneTicket2 from './images/NoneClickTicketBtn2.svg';
 // import "./Main.css";
 
 function Main() {
@@ -11,24 +14,33 @@ function Main() {
       <MainAll>
         <MainWaySelect>
           <MainOneWay>편도</MainOneWay>
-          <MainAroungWay>왕복</MainAroungWay>
+          <MainAroundWay>왕복</MainAroundWay>
         </MainWaySelect>
         <div>
-          {/* <MainGoMentDIV> */}
-          <MainGoMent>출발</MainGoMent>
-          <MainSwitchBtn><img alt="switch_button" src={Switch} /></MainSwitchBtn>
-          <MainArrivedMent>도착</MainArrivedMent>
-          {/* </MainGoMentDIV> */}
-          <MainGoStationDIV>
-            <MainGoStation>
-              서울
-            </MainGoStation>
-            <div className="Main_Go_arrow">-&gt;</div>
-            <MainArrivedStation>
-              부산
-            </MainArrivedStation>
+          <MainGoStationDIVStyle>
+            <MainGoStationDIV>
+              <MainGoMentDIV>
+                <MainGoMent>출발</MainGoMent>
+                <MainGoStation>
+                  서울
+                </MainGoStation>
+              </MainGoMentDIV>
+
+              <ArrowDIV>
+                <MainSwitchBtn><img alt="switctButton" src={Switch} /></MainSwitchBtn>
+                <GoArrow><img alt="blueArrow" src={Arrow} /></GoArrow>
+              </ArrowDIV>
+              <MainArrivedMentDIV>
+                <MainArrivedMent>도착</MainArrivedMent>
+                <MainArrivedStation>
+                  부산
+                </MainArrivedStation>
+              </MainArrivedMentDIV>
+
+            </MainGoStationDIV>
             <MainSelectKTX>KTX역 선택 지도</MainSelectKTX>
-          </MainGoStationDIV>
+          </MainGoStationDIVStyle>
+
         </div>
         <MainGoDIV>
           <MainInfoMent>출발일</MainInfoMent>
@@ -48,13 +60,17 @@ function Main() {
         <div />
         <MainTrainInquire>열차 조회하기</MainTrainInquire>
         <MainTrainbtn>
-          <img alt="Main_Train" src={Train} />
+          <img alt="MainTrain" height="25px" src={Train} />
           <div>승차권예매</div>
         </MainTrainbtn>
-        <MainNoneActbtn>할인 ꞏ 정기권</MainNoneActbtn>
-        <MainNoneActbtn>관광상품</MainNoneActbtn>
+        <MainNoneActbtn>
+          <img alt="NoneClickTicketBtn" src={NoneTicket} />
+        </MainNoneActbtn>
+        <MainNoneActbtn>
+          <img alt="NoneClickTicketBtn2" src={NoneTicket2} />
+        </MainNoneActbtn>
         <MainTicketbtn>
-          <img alt="Main_Ticket" src={Ticket} />
+          <img alt="MainTicket" height="30px" src={Ticket} />
           <div>승차권확인</div>
         </MainTicketbtn>
       </MainAll>
@@ -78,7 +94,7 @@ background-color: white;
 
 
 `;
-const MainAroungWay = styled.div`
+const MainAroundWay = styled.div`
 background-color: white;
 color: #064A87;
 font-size: 25px;
@@ -87,49 +103,103 @@ width: 50%;
 height: 40px;
     padding-top: 5px;
 `;
-// const MainGoMentDIV = styled.div`
-// float:left;
 
-// `;
+// 출발역div
+const MainGoMentDIV = styled.div`
+display:flex;
+width:50%;
+height:40%;
 
+
+flex-direction:column;
+// background-color:red;
+`;
+// 화살표div
+const ArrowDIV = styled.div`
+display:flex;
+width:15%;
+height:40%;
+
+
+flex-direction:column;
+// background-color:yellow;
+
+
+`;
+// 도착역div
+const MainArrivedMentDIV = styled.div`
+display:flex;
+width:50%;
+height:40%;
+
+flex-direction:column;
+// background-color:green;
+
+
+`;
+// 출발역 ment, 역
 const MainGoMent = styled.div`
 font-size: 12px;
 color: #686868;
-// float: left;
-width: 45%;
-`;
+display:flex;
+justify-content:center;
+align-items:center;
+// padding-top:10px;
+// margin-top:5px;
 
-const MainSwitchBtn = styled.div`
-border: 0.5px solid #3F9CF1;
-    border-radius: 10px;
-    padding-left: 10px;
-    padding-right: 10px;
-    width: 10%;
-    // float: left;
 
-`;
-
-const MainArrivedMent = styled.div`
-font-size: 12px;
-    color: #686868;
-    // float: left;
-    width: 45%;
 `;
 const MainGoStation = styled.div`
 color: #064A87;
     font-size: 30px;
     font-weight: bold;
-    float: left;
-    width: 45%;
-    /* margin-top: 20px; */
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    padding-top:10px;
+`;
+
+// 도착역 ment,역
+const MainArrivedMent = styled.div`
+font-size: 12px;
+    color: #686868;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+
+
 `;
 const MainArrivedStation = styled.div`
 color: #064A87;
 font-size: 30px;
 font-weight: bold;
 float:right;
-width: 45%;
+display:flex;
+justify-content:center;
+align-items:center;
+padding-top:10px;
+
 `;
+
+// 화살표 둘
+const MainSwitchBtn = styled.div`
+border: 0.5px solid #3F9CF1;
+    border-radius: 8px;
+    padding-left: 10px;
+    padding-right: 10px;
+    margin-top:5px;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+
+
+
+`;
+
+const GoArrow = styled.div`
+padding-top:15px;
+`;
+
 const MainInfoMent = styled.div`
 color:#3F9CF1
 ;
@@ -160,24 +230,33 @@ const MainNoneActbtn = styled.div`
 background-color: #F9F9F9;
 height:54px;
 float: left;
-/* padding: 10px; */
 width: 25%;
+// margin-top:0px;
 `;
 const MainTicketbtn = styled.button`
 background-color: #F9F9F9;
     border: 0px;
     float: left;
-    /* padding: 10px; */
     width: 25%;
     height:54px;
 
 `;
+// 감싸는 div
 const MainGoStationDIV = styled.div`
 width: 100%;
-height: 140px;
+height: 130px;
+
+
+display:flex;
+justify-content:center;
+align-items:center;
+
+`;
+// 감싸는 div style
+const MainGoStationDIVStyle = styled.div`
 color: #000000;
 border-bottom: 1px solid #686868;
-
+padding-bottom:10px;
 `;
 
 const MainGoDIV = styled.div`
@@ -185,7 +264,12 @@ width: 100%;
     height: 98px;
     color: #000000;
     border-bottom: 1px solid #686868;
-`;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    justify-content:center;
+
+    `;
 const MainSelectKTX = styled.button`
 background-color: white;
     border-radius: 20px;
@@ -193,7 +277,9 @@ background-color: white;
     border: 1px solid #686868;
     width: 90%;
     height: 29px;
+    cursor:pointer;
 `;
+
 const MainAll = styled.div`
 background-color: #F7F7F7;
 text-align:center;
