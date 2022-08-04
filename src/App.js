969bb79990b16ad2f1dc1de853f8cbc6138b3292
구지민 <rnwlals02@naver.com>
2022-08-04 18:08:47 +0900

    feat: 외래어 사전 메인

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import Home from './Components/Home/Home';
+// import Home from './Components/Home/Home';
+import DictMain from './Components/Korail/DictMain';
 
 function App() {
   return (
-    <div>
+    /*<div>
       <Home />
       <p>가치잇다</p>
-    </div>
+    </div>*/
+    <DictMain />
   );
 }
 
---
+++ src/Components/Korail/DictMain.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import styled from 'styled-components';
+import BookImg from './images/BookImg';
+import Enter from './images/Enter';
+
+const Item = ({ text }) => text;
+
+function DictMain() {
+  return (
+    <All>
+      <IconWrap>
+        <BookImg />
+      </IconWrap>{' '}
+      <DictMent> 쉬운 우리말 사전 </DictMent>{' '}
+      <DetailMent>
+        외국어 용어의 <Item text={<br />} />
+        쉬운 우리말 표현 사전{' '}
+      </DetailMent>{' '}
+      <SearchBar>
+        <SearchInput placeholder='검색할 외래어를 입력해주세요.' />
+        <SearchIcon>
+          <Enter />
+        </SearchIcon>{' '}
+      </SearchBar>{' '}
+    </All>
+  );
+}
+
+const All = styled.div`
+  background-color: #eff3fa;
+  padding-top: 60px;
+  padding-bottom: 100%;
+`;
+
+const IconWrap = styled.div`
+  margin: 116px 286px 12px 22px;
+`;
+
+const DictMent = styled.div`
+  font-weight: 700;
+  font-size: 37px;
+  margin: 0px 65px 6px 30px;
+  display: flex;
+`;
+
+const DetailMent = styled.div`
+  font-weight: 400;
+  font-size: 22px;
+  margin: 0px 120px 36px 35px;
+`;
+
+const SearchBar = styled.div`
+  background: #b3e5f2;
+  border-radius: 20px;
+  width: 318px;
+  height: 38px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0px 28px;
+`;
+
+const SearchInput = styled.input`
+  background: transparent;
+  border: none;
+  width: 70%;
+  color: #7a7a7a;
+  font-weight: 400;
+  font-size: 16px;
+`;
+
+const SearchIcon = styled.button`
+  border: none;
+  background: transparent;
+`;
+
+export default DictMain;
