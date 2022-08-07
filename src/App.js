53bab3cc947ba9ba4dfa6fbb2ba52954e1945623
Jieun Lee <najee0610@naver.com>
2022-08-08 00:34:17 +0900

    refactor: DIV 태그 정리, 띄어쓰기 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import Home from './Components/Home/Home';
 import TicketConfirm from './Components/Korail/TicketConfirm';
 
 function App() {
--- src/Components/Korail/TicketConfirm.js
+++ src/Components/Korail/TicketConfirm.js
@@ -8,6 +8,7 @@ import { ReactComponent as Train } from './images/Train.svg';
 import { ReactComponent as Ticket } from './images/Ticket.svg';
 import { ReactComponent as NoneTicket } from './images/NoneClickTicketBtn.svg';
 import { ReactComponent as NoneTicket2 } from './images/NoneClickTicketBtn2.svg';
+
 function TicketConfirm() {
   return (
     <All>
@@ -82,6 +83,7 @@ function TicketConfirm() {
     </All>
   );
 }
+
 const All = styled.div`
   text-align: center;
   display: flex;
@@ -90,9 +92,9 @@ const All = styled.div`
   margin-top: 64px;
   width: 100%;
   height: 100%;
-
   background-color: #ededed;
 `;
+
 const ConfirmHeaderWrap = styled.div``;
 
 const ConfirmHeader = styled.div`
@@ -103,11 +105,11 @@ const ConfirmHeader = styled.div`
   width: 50%;
   height: 45px;
   border-bottom: 2px solid #064a87;
-
   display: flex;
   align-items: center;
   justify-content: center;
 `;
+
 const ConfirmHeader2 = styled.div`
   background-color: white;
   color: #686868;
@@ -115,16 +117,15 @@ const ConfirmHeader2 = styled.div`
   float: right;
   width: 50%;
   height: 45px;
-
   display: flex;
   align-items: center;
   justify-content: center;
 `;
+
 const Day = styled.div`
   background-color: #064a87;
   color: #ffffff;
   border-radius: 5px 5px 0 0;
-  /* float: left; */
   width: 200px;
   height: 36px;
   display: flex;
@@ -143,20 +144,24 @@ const StationDIV = styled.div`
   align-items: center;
   margin-bottom: 10px;
 `;
+
 const GoStation = styled.div`
   color: #064a87;
   font-weight: bold;
   font-size: 30px;
 `;
+
 const GoTime = styled.div`
   color: #686868;
   font-size: 12px;
 `;
+
 const ArrivedStation = styled.div`
   color: #064a87;
   font-weight: bold;
   font-size: 30px;
 `;
+
 const ArrivedTime = styled.div`
   color: #686868;
   font-size: 12px;
@@ -165,33 +170,33 @@ const ArrivedTime = styled.div`
 const GoWrap = styled.div`
   height: 105px;
   width: 139px;
-  /* background-color: red; */
   display: flex;
   flex-direction: column;
   text-align: center;
   justify-content: center;
 `;
+
 const ArrivedWrap = styled.div`
   height: 105px;
   width: 139px;
-  /* background-color: yellow; */
   display: flex;
   flex-direction: column;
   text-align: center;
   justify-content: center;
 `;
+
 const ArrowWrap = styled.div`
-  /* height: 105px; */
   width: 80px;
-  /* background-color: green; */
   display: flex;
   text-align: center;
   justify-content: center;
 `;
+
 const StationDIVWrap = styled.div`
   display: flex;
   justify-content: center;
 `;
+
 const TrainNameDIV = styled.div`
   background-color: #e1e9f6;
   height: 52px;
@@ -203,18 +208,22 @@ const TrainNameDIV = styled.div`
   width: 90%;
   margin-left: 10px;
 `;
+
 const TrainName = styled.div`
   font-size: 16px;
 `;
+
 const SeatDIV = styled.div`
   width: 95%;
   margin-left: 10px;
 `;
+
 const SeatTitleDIV = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
 `;
+
 const SeatTitle = styled.div`
   background-color: #d9d9d9;
   font-size: 9px;
@@ -226,25 +235,30 @@ const SeatTitle = styled.div`
   align-items: center;
   justify-content: center;
 `;
+
 const SeatContentsDIV = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   border-bottom: 0.65px solid #686868;
 `;
+
 const TrainNum = styled.div`
   font-size: 30px;
   color: #064a87;
 `;
+
 const TrainNumFix = styled.div`
   font-size: 13px;
   color: #064a87;
   padding-bottom: 4px;
 `;
+
 const SeatNum = styled.div`
   font-size: 30px;
   color: #064a87;
 `;
+
 const TrainNumWrap = styled.div`
   display: flex;
   align-items: flex-end;
@@ -258,6 +272,7 @@ const SeatContentsWrap = styled.div`
   justify-content: center;
   align-items: center;
 `;
+
 const MainTrainbtn = styled.button`
   background-color: #f9f9f9;
   border: 0px;
@@ -265,12 +280,14 @@ const MainTrainbtn = styled.button`
   width: 25%;
   height: 54px;
 `;
+
 const MainNoneActbtn = styled.div`
   background-color: #f9f9f9;
   height: 54px;
   float: left;
   width: 25%;
 `;
+
 const MainTicketbtn = styled.button`
   background-color: #f9f9f9;
   border: 0px;
@@ -286,4 +303,5 @@ const ConfirmFooter = styled.div`
   background-color: #f9f9f9;
   width: 100%;
 `;
+
 export default TicketConfirm;
