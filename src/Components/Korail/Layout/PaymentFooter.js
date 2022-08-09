6744d7cee7d363c19f98fd2a9f1e46189b7ff297
Jieun Lee <najee0610@naver.com>
2022-08-09 15:59:48 +0900

    style: 띄어쓰기

--- src/Components/Korail/Layout/PaymentFooter.js
+++ src/Components/Korail/Layout/PaymentFooter.js
@@ -15,18 +15,21 @@ function PaymentFooter() {
     </FooterDiv>
   );
 }
+
 const Number = styled.div`
   font-size: 15px;
   font-weight: bold;
   color: #b3d5f2;
   padding-left: 10px;
 `;
+
 const Cost = styled.div`
   font-size: 15px;
   font-weight: bold;
   color: #ffffff;
   padding-left: 260px;
 `;
+
 const CostDiv = styled.div`
   background-color: #000000;
   opacity: 70%;
@@ -35,6 +38,7 @@ const CostDiv = styled.div`
   display: flex;
   align-items: center;
 `;
+
 const Cancel = styled.div`
   font-size: 20px;
   font-weight: bold;
@@ -44,6 +48,7 @@ const Cancel = styled.div`
   align-items: center;
   justify-content: center;
 `;
+
 const Next = styled.div`
   font-size: 20px;
   font-weight: bold;
@@ -54,11 +59,13 @@ const Next = styled.div`
   justify-content: center;
   border: 5px solid #3f9cf1;
 `;
+
 const BtnDiv = styled.div`
   background-color: #c6dfee;
   display: flex;
   height: 42px;
 `;
+
 const FooterDiv = styled.div`
   position: fixed;
   width: 100%;
