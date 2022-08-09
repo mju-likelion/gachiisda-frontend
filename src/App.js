72f9ca57a1eba4261d12ab5a958750e7c68f2533
Jieun Lee <najee0610@naver.com>
2022-08-09 17:00:25 +0900

    feat: App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import Main from './Components/Korail/Beginner/BGMain';
+// import Home from './Components/Home/Home';
+// import Header from './Components/Korail/Layout/Header';
+import PeopleSection from './Components/Korail/BeginnerPeopleSection';
 
 function App() {
-  return <Main />;
+  return (
+    <div>
+      {/* <Header />
+      <Home />
+      <p>가치잇다</p> */}
+      <PeopleSection />
+    </div>
+  );
 }
 
 export default App;
