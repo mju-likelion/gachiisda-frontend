125851baf7865bc15fb102d72c0c4568bdaabc46
구지민 <rnwlals02@naver.com>
2022-08-10 20:06:54 +0900

    refactor: merge 전 오류 수정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-// import Home from './Components/Home/Home';
-import Footer from './Components/Korail/Footer';
+import Home from './Components/Home/Home';
+import Header from './Components/Korail/Layout/Header';
 
 function App() {
   return (
-    /*<div>
+    <div>
+      <Header />
       <Home />
       <p>가치잇다</p>
-    </div>*/
-    <Footer />
+    </div>
   );
 }
 
