0c98121672663c805ea0e09bb4296c9b3e1dc050
0727ha <mcd158@naver.com>
2022-08-11 16:08:00 +0900

    fix: seat 수정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import Home from './Components/Home/Home';
-import Header from './Components/Korail/Layout/Header';
+//import Home from './Components/Home/Home';
+//import Header from './Components/Korail/Layout/Header';
+import Korail from './Components/Korail/ChooseSection';
 
 function App() {
   return (
     <div>
-      <Header />
-      <Home />
+      {/* <Header /> */}
+      <Korail />
       <p>가치잇다</p>
     </div>
   );
