2902de426efbe3d347750b087edcd92d0407d989
Jieun Lee <najee0610@naver.com>
2022-08-17 15:23:48 +0900

    fix: 배포 오류 수정

--- src/Components/Korail/Beginner/BGMain.js
+++ src/Components/Korail/Beginner/BGMain.js
@@ -57,7 +57,6 @@ function BGMain() {
       // setDay();
       setDate(response.data.data.dates);
       setTime(response.data.data.timeTable);
-      console.log(response.data.data.dates);
     });
   }, []);
 
